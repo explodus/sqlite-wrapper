80951de038d6aca2cfb81dea93db6958f72572e6
Torsten Schroeder <schroeder@ipe-chemnitz.de>
2010-03-17 10:12:37 +0100

    little more linux fixes

--- include/sqlite_wrapper/sqlite/sqlite3.h
+++ include/sqlite_wrapper/sqlite/sqlite3.h
@@ -247,6 +247,9 @@ typedef struct sqlite3 sqlite3;
 #ifdef SQLITE_INT64_TYPE
   typedef SQLITE_INT64_TYPE sqlite_int64;
   typedef unsigned SQLITE_INT64_TYPE sqlite_uint64;
+#elif defined(SQLITE_WRAPPER_HAVE_ATLEAST_UINT64)
+	typedef boost::int_least64_t sqlite_int64;
+	typedef boost::uint_least64_t sqlite_uint64;
 #elif defined(_MSC_VER) || defined(__BORLANDC__)
   typedef __int64 sqlite_int64;
   typedef unsigned __int64 sqlite_uint64;
