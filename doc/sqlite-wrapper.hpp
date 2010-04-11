43c62ba186a81d2b9d5f1bd862cfd7f73fae655d
explodus <explodus@gmx.de>
2010-04-11 14:31:48 +0200

    docu update

--- doc/sqlite-wrapper.hpp
+++ doc/sqlite-wrapper.hpp
@@ -7,9 +7,10 @@
 /// This file is part of the sqlite_wrapper project
 ///
 
-/// @defgroup sqlite_wrapper Db Wrapper around Sqlite
+/// @defgroup sqlite_wrapper wrapper
+/// @defgroup sqlite_wrapper_examples examples
 ///
-/// @mainpage Db Wrapper around Sqlite
+/// @mainpage Wrapper around Sqlite with logging facilities
 /// @ingroup sqlite_wrapper
 /// @section source Getting the source
 ///
@@ -21,49 +22,49 @@
 /// 	- CMake 2.6
 /// 	- Boost 1.38
 ///
+/// @ref sqlite_wrapper_examples
+///
 /// @section main_todo Todo
 ///
 /// @copydoc todo
 ///
-/// @}
 
 /// @page update usage example of the wrapper
-/// @ingroup sqlite_wrapper
+/// @ingroup sqlite_wrapper_examples
 ///
 /// only a simple example
 ///
 /// @code
-///try
-///{
-///	db::base_ptr _db(get_db());
-///	
-///	if (_db==0)
-///		return;
-///
-///	double anl_id(db::detail::to_type<double>(db::string(_code)));
-///	db::string kenst(DB_TEXT("Blub"));
-///
-///	_db->execute_ptr(
-///		(db::upd(L"TermChange") % 
-///			db::field(L"KENST", kenst) %
-///			(
-///				db::field(L"ANL_ID", 0.0) == anl_id && 
-///				db::field(L"NR", 0) == _nr
-///			)
-///		)
-///	);
-///}
-///catch (db::exception::base& e)
-///{
-///		db::log::singleton::basic::log_msg msg(
-///			db::log::singleton::basic::get_log()
-///		, e.what() 
-///		, db::log::log_error);
-///	return;
-///}
+/// try
+/// {
+/// 	db::base_ptr _db(get_db());
+/// 	
+/// 	if (_db==0)
+/// 		return;
+/// 
+/// 	double anl_id(db::detail::to_type<double>(db::string(_code)));
+/// 	db::string kenst(DB_TEXT("Blub"));
+/// 
+/// 	_db->execute_ptr(
+/// 		(db::upd(L"TermChange") % 
+/// 			db::field(L"KENST", kenst) %
+/// 			(
+/// 				db::field(L"ANL_ID", 0.0) == anl_id && 
+/// 				db::field(L"NR", 0) == _nr
+/// 			)
+/// 		)
+/// 	);
+/// }
+/// catch (db::exception::base& e)
+/// {
+/// 		db::log::singleton::basic::log_msg msg(
+/// 			db::log::singleton::basic::get_log()
+/// 		, e.what() 
+/// 		, db::log::log_error);
+/// 	return;
+/// }
 /// @endcode
 ///
-/// @ref sqlite_wrapper
 
 #include <config.hpp>
 namespace db {}
