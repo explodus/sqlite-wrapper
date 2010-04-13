52ee9e61c795d90056ca04159b595dcd77818d00
Torsten Schroeder <schroeder@ipe-chemnitz.de>
2010-04-13 16:10:07 +0200

    improve the db_log_viewer

--- test/db_log_viewer.cpp
+++ test/db_log_viewer.cpp
@@ -10,128 +10,275 @@
 ///
 
 #include <sqlite_wrapper/config.hpp>
+#include <sqlite_wrapper/db.hpp>
+#include <sqlite_wrapper/log.hpp>
+
 #include <QApplication>
 #include <QDialog>
 #include <QtGui>
 
-class dialog : public QDialog
+#include "db_log_viewer.hpp"
+
+namespace db
 {
-	//Q_OBJECT
+	namespace detail
+	{
+		inline QString internal_convert( std::string const &_s )
+		{  return QString( QByteArray( _s.c_str() ) ); }
 
-public:
-	dialog();
+		inline QString internal_convert( std::wstring const &_s )
+		{ return QString::fromUtf16( reinterpret_cast<const ushort*>( _s.c_str() ), _s.length() ); }
+	}
 
-private:
-	void createMenu();
-	void createHorizontalGroupBox();
-	void createGridGroupBox();
-	void createFormGroupBox();
+	inline QString to_qstring( db::string const &_s )
+	{ return detail::internal_convert(_s); }
+}
 
-	enum { NumGridRows = 3, NumButtons = 4 };
 
-	QMenuBar *menuBar;
-	QGroupBox *horizontalGroupBox;
-	QGroupBox *gridGroupBox;
-	QGroupBox *formGroupBox;
-	QTextEdit *smallEditor;
-	QTextEdit *bigEditor;
-	QLabel *labels[NumGridRows];
-	QLineEdit *lineEdits[NumGridRows];
-	QPushButton *buttons[NumButtons];
-	QDialogButtonBox *buttonBox;
+QVariant db_log_model::headerData( 
+	  int section
+	, Qt::Orientation orientation
+	, int role ) const
+{
+	QVariant tmp;
 
-	QMenu *fileMenu;
-	QAction *exitAction;
-};
+	if (orientation == Qt::Horizontal && role == Qt::DisplayRole) 
+	{
+		switch (section) 
+		{
+			case COLUMN_ID:
+				tmp = tr("#");
+				break;
+			case COLUMN_DATE_REAL:
+				tmp = tr("DateReal");
+				break;
+			case COLUMN_DATE_TEXT:
+				tmp = tr("DateText");
+				break;
+			case COLUMN_LEVEL:
+				tmp = tr("Level");
+				break;
+			case COLUMN_MSG:
+				tmp = tr("Message");
+				break;
+			default:
+				qCritical() << __FUNCTION__ << "Error: unknown column:" << section;
+		}
+	}
+
+	return tmp;
+}
+
+QVariant db_log_model::data( const QModelIndex & index, int role /*= Qt::DisplayRole*/ ) const
+{
+	static QVariant empty_tmp;
+
+	if (!index.isValid() || index.model() != this) 
+		return empty_tmp;
+
+	if (!_q)
+		return empty_tmp;
+
+	if (_q->size() == 0)
+		return empty_tmp;
+
+	int row(index.row()), column(index.column());
+
+	if (role == Qt::DisplayRole) 
+	{
+		static QVariant filled_tmp;
+
+		switch (column)
+		{
+		case db_log_model::COLUMN_LEVEL:
+			switch (_q->getRow(row).value(column).get_int())
+			{
+			case db::log::log_critical:
+				filled_tmp = QString("critical");
+				break;
+			case db::log::log_error:
+				filled_tmp = QString("error");
+				break;
+			case db::log::log_warning:
+				filled_tmp = QString("warning");
+				break;
+			case db::log::log_notice:
+				filled_tmp = QString("notice");
+				break;
+			case db::log::log_info:
+				filled_tmp = QString("info");
+				break;
+			case db::log::log_debug:
+				filled_tmp = QString("debug");
+				break;
+			default:
+				filled_tmp = QString("undef");
+				break;
+			}
+			break;
+		default :
+			filled_tmp = db::to_qstring(_q->getRow(row).value(column).str());
+			break;
+		}
+		return filled_tmp;
+	}
+
+	
+	return empty_tmp;
+}
 
 int main( int argc, char **argv )
 {
 	QApplication app(argc, argv);
+	QStringList arguments = app.arguments();
+	QString app_path = app.applicationDirPath();
+
+	std::string log_db = app_path.toAscii();
+	log_db += "/log.db";
 
 	dialog dlg;
-	return dlg.exec();
+	db_log_model *model = new db_log_model(&dlg);
+
+	db::base_ptr db_; 
+	try
+	{
+		db_.reset(new db::base);
+		db_->connect(log_db.c_str());
+		model->Q(db_->execute_ptr(
+			(
+					db::sel(L"log")
+				, L"id"
+				, L"date_real"
+				, L"date_text"
+				, L"level"
+				, L"msg"
+			).limit(1000).order_by(L"date_real", false)
+		));
+	}
+	catch (db::exception::base& e)
+	{
+		db::log::singleton::basic::log_msg msg(
+			  db::log::singleton::basic::get_log()
+		  , e.what()
+			, db::log::log_error);
+	}
+
+	dlg.setSourceModel(model);
+	dlg.show();
+	return app.exec();
 }
 
 dialog::dialog()
 {
-	createMenu();
-	createHorizontalGroupBox();
-	createGridGroupBox();
-	createFormGroupBox();
+	proxyModel = new QSortFilterProxyModel;
+	proxyModel->setDynamicSortFilter(true);
 
-	bigEditor = new QTextEdit;
-	bigEditor->setPlainText(tr("This widget takes up all the remaining space "
-		"in the top-level layout."));
+	sourceGroupBox = new QGroupBox(tr("Original Model"));
+	proxyGroupBox = new QGroupBox(tr("Sorted/Filtered Model"));
 
-	buttonBox = new QDialogButtonBox(QDialogButtonBox::Ok
-		| QDialogButtonBox::Cancel);
+	sourceView = new QTreeView;
+	sourceView->setRootIsDecorated(false);
+	sourceView->setAlternatingRowColors(true);
 
-	connect(buttonBox, SIGNAL(accepted()), this, SLOT(accept()));
-	connect(buttonBox, SIGNAL(rejected()), this, SLOT(reject()));
+	proxyView = new QTreeView;
+	proxyView->setRootIsDecorated(false);
+	proxyView->setAlternatingRowColors(true);
+	proxyView->setModel(proxyModel);
+	proxyView->setSortingEnabled(true);
+
+	sortCaseSensitivityCheckBox = new QCheckBox(tr("Case sensitive sorting"));
+	filterCaseSensitivityCheckBox = new QCheckBox(tr("Case sensitive filter"));
+
+	filterPatternLineEdit = new QLineEdit;
+	filterPatternLabel = new QLabel(tr("&Filter pattern:"));
+	filterPatternLabel->setBuddy(filterPatternLineEdit);
+
+	filterSyntaxComboBox = new QComboBox;
+	filterSyntaxComboBox->addItem(tr("Regular expression"), QRegExp::RegExp);
+	filterSyntaxComboBox->addItem(tr("Wildcard"), QRegExp::Wildcard);
+	filterSyntaxComboBox->addItem(tr("Fixed string"), QRegExp::FixedString);
+	filterSyntaxLabel = new QLabel(tr("Filter &syntax:"));
+	filterSyntaxLabel->setBuddy(filterSyntaxComboBox);
+
+	filterColumnComboBox = new QComboBox;
+	filterColumnComboBox->addItem(tr("#"));
+	filterColumnComboBox->addItem(tr("DateReal"));
+	filterColumnComboBox->addItem(tr("DateText"));
+	filterColumnComboBox->addItem(tr("Level"));
+	filterColumnComboBox->addItem(tr("Message"));
+	filterColumnLabel = new QLabel(tr("Filter &column:"));
+	filterColumnLabel->setBuddy(filterColumnComboBox);
+
+	connect(filterPatternLineEdit, SIGNAL(textChanged(QString)),
+		this, SLOT(filterRegExpChanged()));
+	connect(filterSyntaxComboBox, SIGNAL(currentIndexChanged(int)),
+		this, SLOT(filterRegExpChanged()));
+	connect(filterColumnComboBox, SIGNAL(currentIndexChanged(int)),
+		this, SLOT(filterColumnChanged()));
+	connect(filterCaseSensitivityCheckBox, SIGNAL(toggled(bool)),
+		this, SLOT(filterRegExpChanged()));
+	connect(sortCaseSensitivityCheckBox, SIGNAL(toggled(bool)),
+		this, SLOT(sortChanged()));
+
+	QHBoxLayout *sourceLayout = new QHBoxLayout;
+	sourceLayout->addWidget(sourceView);
+	sourceGroupBox->setLayout(sourceLayout);
+
+	QGridLayout *proxyLayout = new QGridLayout;
+	proxyLayout->addWidget(proxyView, 0, 0, 1, 3);
+	proxyLayout->addWidget(filterPatternLabel, 1, 0);
+	proxyLayout->addWidget(filterPatternLineEdit, 1, 1, 1, 2);
+	proxyLayout->addWidget(filterSyntaxLabel, 2, 0);
+	proxyLayout->addWidget(filterSyntaxComboBox, 2, 1, 1, 2);
+	proxyLayout->addWidget(filterColumnLabel, 3, 0);
+	proxyLayout->addWidget(filterColumnComboBox, 3, 1, 1, 2);
+	proxyLayout->addWidget(filterCaseSensitivityCheckBox, 4, 0, 1, 2);
+	proxyLayout->addWidget(sortCaseSensitivityCheckBox, 4, 2);
+	proxyGroupBox->setLayout(proxyLayout);
 
 	QVBoxLayout *mainLayout = new QVBoxLayout;
-	mainLayout->setMenuBar(menuBar);
-	mainLayout->addWidget(horizontalGroupBox);
-	mainLayout->addWidget(gridGroupBox);
-	mainLayout->addWidget(formGroupBox);
-	mainLayout->addWidget(bigEditor);
-	mainLayout->addWidget(buttonBox);
+	mainLayout->addWidget(sourceGroupBox);
+	mainLayout->addWidget(proxyGroupBox);
 	setLayout(mainLayout);
 
-	setWindowTitle(tr("Basic Layouts"));
+	setWindowTitle(tr("Basic Sort/Filter Model"));
+	resize(600, 800);
+
+	proxyView->sortByColumn(1, Qt::AscendingOrder);
+	filterColumnComboBox->setCurrentIndex(3);
+
+	filterPatternLineEdit->setText("error");
+	filterCaseSensitivityCheckBox->setChecked(true);
+	sortCaseSensitivityCheckBox->setChecked(true);
 }
 
-void dialog::createMenu()
+void dialog::setSourceModel(QAbstractItemModel *model)
 {
-	menuBar = new QMenuBar;
-
-	fileMenu = new QMenu(tr("&File"), this);
-	exitAction = fileMenu->addAction(tr("E&xit"));
-	menuBar->addMenu(fileMenu);
-
-	connect(exitAction, SIGNAL(triggered()), this, SLOT(accept()));
+	proxyModel->setSourceModel(model);
+	sourceView->setModel(model);
 }
 
-void dialog::createHorizontalGroupBox()
+void dialog::filterRegExpChanged()
 {
-	horizontalGroupBox = new QGroupBox(tr("Horizontal layout"));
-	QHBoxLayout *layout = new QHBoxLayout;
+	QRegExp::PatternSyntax syntax =
+		QRegExp::PatternSyntax(filterSyntaxComboBox->itemData(
+		filterSyntaxComboBox->currentIndex()).toInt());
+	Qt::CaseSensitivity caseSensitivity =
+		filterCaseSensitivityCheckBox->isChecked() ? Qt::CaseSensitive
+		: Qt::CaseInsensitive;
 
-	for (int i = 0; i < NumButtons; ++i) {
-		buttons[i] = new QPushButton(tr("Button %1").arg(i + 1));
-		layout->addWidget(buttons[i]);
-	}
-	horizontalGroupBox->setLayout(layout);
+	QRegExp regExp(filterPatternLineEdit->text(), caseSensitivity, syntax);
+	proxyModel->setFilterRegExp(regExp);
 }
 
-void dialog::createGridGroupBox()
+void dialog::filterColumnChanged()
 {
-	gridGroupBox = new QGroupBox(tr("Grid layout"));
-	QGridLayout *layout = new QGridLayout;
-
-	for (int i = 0; i < NumGridRows; ++i) {
-		labels[i] = new QLabel(tr("Line %1:").arg(i + 1));
-		lineEdits[i] = new QLineEdit;
-		layout->addWidget(labels[i], i + 1, 0);
-		layout->addWidget(lineEdits[i], i + 1, 1);
-	}
-
-	smallEditor = new QTextEdit;
-	smallEditor->setPlainText(tr("This widget takes up about two thirds of the "
-		"grid layout."));
-	layout->addWidget(smallEditor, 0, 2, 4, 1);
-
-	layout->setColumnStretch(1, 10);
-	layout->setColumnStretch(2, 20);
-	gridGroupBox->setLayout(layout);
+	proxyModel->setFilterKeyColumn(filterColumnComboBox->currentIndex());
 }
 
-void dialog::createFormGroupBox()
+void dialog::sortChanged()
 {
-	formGroupBox = new QGroupBox(tr("Form layout"));
-	QFormLayout *layout = new QFormLayout;
-	layout->addRow(new QLabel(tr("Line 1:")), new QLineEdit);
-	layout->addRow(new QLabel(tr("Line 2, long text:")), new QComboBox);
-	layout->addRow(new QLabel(tr("Line 3:")), new QSpinBox);
-	formGroupBox->setLayout(layout);
+	proxyModel->setSortCaseSensitivity(
+		sortCaseSensitivityCheckBox->isChecked() ? Qt::CaseSensitive
+		: Qt::CaseInsensitive);
 }
---
+++ test/db_log_viewer.hpp
@@ -0,0 +1,127 @@
+///	@file test/db_log_viewer.hpp
+///  
+///	@date 16.3.2010 19:51
+///  
+/// @author T. Schroeder (explodus@gmx.de)
+///  
+/// @brief
+///  
+/// This file is part of the sqlite-wrapper project
+///
+
+#ifndef SQLITE_WRAPPER_TEST_DB_LOG_VIEWER_HPP_INCLUDED
+#define SQLITE_WRAPPER_TEST_DB_LOG_VIEWER_HPP_INCLUDED
+
+#include <sqlite_wrapper/config.hpp>
+#include <sqlite_wrapper/detail/query.hpp>
+
+#include <QWidget>
+#include <QtCore/QAbstractItemModel>
+
+class QCheckBox;
+class QComboBox;
+class QGroupBox;
+class QLabel;
+class QLineEdit;
+class QSortFilterProxyModel;
+class QTreeView;
+
+class db_log_model : public QAbstractItemModel 
+{
+	db::query_ptr _q;
+
+	Q_OBJECT
+public:
+	enum
+	{
+			COLUMN_FIRST = -1
+		, COLUMN_ID
+		, COLUMN_DATE_REAL
+		, COLUMN_DATE_TEXT
+		, COLUMN_LEVEL
+		, COLUMN_MSG
+		, COLUMN_LAST
+	};
+
+	db_log_model(QObject *parent = 0) : QAbstractItemModel(parent)
+	{
+
+	}
+
+	virtual ~db_log_model() { }
+
+	db::query_ptr Q() const { return _q; }
+	void Q(db::query_ptr val) { _q = val; }
+
+	int columnCount(const QModelIndex & parent = QModelIndex()) const
+	{ UNREFERENCED_PARAMETER(parent); return COLUMN_LAST; }
+
+	int rowCount(const QModelIndex & parent = QModelIndex()) const
+	{
+		if (parent.isValid()) 
+			return 0;
+
+		if (!_q)
+			return 0;
+
+		return _q->size();
+	}
+
+	QVariant headerData(int section, Qt::Orientation orientation, int role = Qt::DisplayRole) const;
+	QVariant data(const QModelIndex & index, int role = Qt::DisplayRole) const;
+
+	QModelIndex index(int row, int column, const QModelIndex & parent = QModelIndex()) const
+	{
+		if (parent.isValid()) 
+			return QModelIndex();
+
+		if (!hasIndex(row, column, parent)) 
+			return QModelIndex();
+
+		return createIndex(row, column);
+	}
+	
+	QModelIndex parent(const QModelIndex & index) const
+	{ return QModelIndex(); }
+
+	Qt::ItemFlags flags(const QModelIndex & index) const
+	{
+		if (!index.isValid()) 
+			return Qt::ItemIsDropEnabled;
+
+		return Qt::ItemIsEnabled | Qt::ItemIsSelectable | Qt::ItemIsDragEnabled;
+	}
+};
+
+class dialog : public QWidget
+{
+	Q_OBJECT
+
+public:
+	dialog();
+
+	void setSourceModel(QAbstractItemModel *model);
+
+	private slots:
+		void filterRegExpChanged();
+		void filterColumnChanged();
+		void sortChanged();
+
+private:
+	QSortFilterProxyModel *proxyModel;
+
+	QGroupBox *sourceGroupBox;
+	QGroupBox *proxyGroupBox;
+	QTreeView *sourceView;
+	QTreeView *proxyView;
+	QCheckBox *filterCaseSensitivityCheckBox;
+	QCheckBox *sortCaseSensitivityCheckBox;
+	QLabel *filterPatternLabel;
+	QLabel *filterSyntaxLabel;
+	QLabel *filterColumnLabel;
+	QLineEdit *filterPatternLineEdit;
+	QComboBox *filterSyntaxComboBox;
+	QComboBox *filterColumnComboBox;
+};
+
+#endif
